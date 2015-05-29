7c8d53e3d6fded4a54f7d454a6adf48ef64e03ce
ViktorNova <viktor@pinknoiseboys.com>
2015-05-28 19:53:56 -0700

    It all works - except for NSM.. the NSM OSC command at the very end!

--- chronotrigger.py
+++ chronotrigger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # Switch to the next song in NSM when Jack transport reaches a certain point
 # __author__ = 'Viktor Nova'
+# from _ast import arg
+import liblo
 
 import nsmclient            # Local copy from latest git master: https://raw.githubusercontent.com/nilsgey/pynsmclient/master/nsmclient.py
 import jack                 # Local copy from latest git master: https://raw.githubusercontent.com/spatialaudio/jackclient-python/master/jack.py
@@ -16,12 +18,12 @@ import argparse
 
 # Get song position to switch at and next song to automatically switch to
 parser = argparse.ArgumentParser()
-parser.add_argument("bar")      # First argument (Bar where song ends. When JACK transport reaches this bar, the song is considered to be over, and we will switch sessions and load the next song)
+parser.add_argument("endbar")   # First argument (Bar where song ends. When JACK transport reaches this bar, the song is considered to be over, and we will switch sessions and load the next song)
 parser.add_argument("nextsong") # Second argument  (Name of the song we will automatically switch to when this song is finished)
 args = parser.parse_args()
 
-client = jack.Client("chronotrigger")
-client.activate()
+endbar = int(args.endbar)
+nextsong = args.nextsong
 
 NSM_URL = os.getenv('NSM_URL')
 #NSM_URL = "osc.udp://datakTARR:17545/" # for testing - change this to your current NSM url. This changes each time nsmd is launched, you can find it out by adding xterm to your session, then running "echo $NSM_URL"
@@ -32,36 +34,55 @@ if not NSM_URL:
 
 print("NSM Daemon found at ", NSM_URL)
 
-# Give clients a chance to load             TODO: Query nsmd to get all_clients_are_loaded = True instead (if running under NSM)
-sleep(5)
+# Connect to JACK server
+client = jack.Client("chronotrigger")
+client.activate()
+
+# Go to the beginning of the song
+jack.Client.transport_locate(client, 1)
+bar = 1
 
 
 
 # END TEMPORARY SECTION
 # --------------------------------------------------------------------
 
-#Go to the beginning of the song
-jack.Client.transport_locate(client, 1)
+print("Current song position: Bar ", bar)
+print("Song ends at bar ", endbar)
+print("Switching to next song '", nextsong, "' in ", (endbar - bar), "bars")
 
-#Start the transport
+# Give clients a chance to load   TODO: Query nsmd to get all_clients_are_loaded = True instead (if running under NSM)
+sleep(5)
+
+# Start the transport
+print("STARTING TRANSPORT")
 jack.Client.transport_start(client)
 
-#####################################################
-
-# Test - return a human readable time
-while True:
+# Wait for the song to end
+while bar < endbar:
+    print(" ")
     # Print everything for the hell of it
     transport = client.transport_query()
-    print("Full JACK Transport Status: ", transport)
+    bar = transport[1]['bar']
+    # print("Full JACK Transport Status: ", transport)
 
     status = client.transport_state
     print("Transport state is: ", status)
 
-    bar = transport[1]['bar']
-    print("Song position is: ", bar)
+    print("Current song position: Bar ", bar)
+    print("Song ends at bar ", endbar)
+    print("Switching to next song '", nextsong, "' in ", (endbar - bar), "bars")
     sleep(1)
 
+print()
+print("Song is over.")
+print("Disconnecting from JACK")
 
 # TODO: Make this trigger on KILL and HUP as well
 client.deactivate()
 client.close()
+
+print("Closing session and loading next song '", nextsong, "' NOW!")
+
+message = liblo.Message("/nsm/open/", "nextsong")
+liblo.send(NSM_URL, message)
